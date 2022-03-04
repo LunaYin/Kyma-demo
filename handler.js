d4caf30f41c72aa082bdabbf7d5863a1733d6db4
i307132 <luna.yin@sap.com>
2022-03-04 15:13:34 +0800

    add proto and package.json

--- handler.js
+++ handler.js
@@ -1,38 +1,10 @@
-const express = require('express');
-const bodyParser = require("body-parser");
-
-
 module.exports = {
     main: function (event, context) {
+        let jsonstring = event.extensions.request.body.toString();
+        let value = JSON.parse(jsonstring);
+        output = value.input + 1;
         res = event.extensions.response;
-        res.send('OKKKKK');
+        res.send({"output": output});
         return
-        // const app = express();
-        // const port = 3000;
-
-        // app.use(bodyParser.urlencoded({ extended: false }));
-        // app.use(bodyParser.json());
-
-
-        // app.get('/v1/get', (req, res) => {
-        //     res.send('OK')
-        // });
-        // app.post('/v1/post', (req, res) => {
-        //     let input = getInput(req.body);
-        //     let output = generateOutput(req.body);
-        //     console.log('Input: ' + input + 'and Output' + output);
-        //     res.send({ "Output": output });
-        // })
-        // var server = app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
-        // app.stop = function () {
-        //     server.close();
-        // };
-    }
-};
-
-// function getInput(body) {
-//     return body.input;
-// }
-// function generateOutput(body) {
-//     return body.input + 1;
-// }
+        }
+}
